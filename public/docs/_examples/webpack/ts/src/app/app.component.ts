f6ce80e8899b2cc7ac42a4b2b02108e62d54d497
Will Buck <william.parker.buck@gmail.com>
2016-07-29 09:11:26 -0500

    Fixes webpack example error with typescript styles array in app component

--- public/docs/_examples/webpack/ts/src/app/app.component.ts
+++ public/docs/_examples/webpack/ts/src/app/app.component.ts
@@ -6,7 +6,7 @@ import '../../public/css/styles.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [String('./app.component.css')]
 })
 export class AppComponent { }
 // #enddocregion
